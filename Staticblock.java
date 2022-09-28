949dce581a3207c743c188f43f22cb36678df1f6
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-09-28 13:12:36 +0530

    Add files via upload

---
+++ Staticblock.java
@@ -0,0 +1,18 @@
+package Practical.com;
+
+public class Staticblock {
+	/*static int square(int x) {//static method
+		return x*x;
+	}
+public static void main(String[] args) {
+	int result=Squarestatic.square(4);//taking a variable and class name.method name(pass the value)
+	System.out.println(result);
+}
+}*/
+	//static block
+	static {
+		System.out.println("static block");}
+	public static void main(String[] args) {
+		System.out.println("main method");
+	}
+	}
