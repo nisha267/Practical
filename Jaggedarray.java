8d5f13393918965e3b3aadabac454b80ea4098b9
Nisha Sutradhar <111556178+nisha267@users.noreply.github.com>
2022-11-01 13:29:16 +0530

    Add files via upload

---
+++ Jaggedarray.java
@@ -0,0 +1,22 @@
+package Practical.com;
+
+public class Jaggedarray {
+public static void main(String[] args) {
+	//declaring 2d array
+	int arr[][]=new int[3][];//declare no of row=3 but column=blank
+	//defining column 5+7+9=21
+	arr[0]=new int[5];
+	arr[1]=new int[7];
+	arr[2]=new int[9];
+	//initialized the jagged array
+	int count=0;
+	for(int i=0;i<arr.length;i++)//row
+		for(int j=0;j<arr[i].length;j++)//column}
+arr[i][j]=count++;
+	//print the jagged array
+	for(int i=0;i<arr.length;i++){//row
+		for(int j=0;j<arr[i].length;j++){//column
+System.out.println(arr[i][j]+" ");
+}
+System.out.println();
+}}}
